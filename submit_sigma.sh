70703176b86361b8f6c07cdc9d98e270a254ad7b
Casey Smith Cluster <cberger@smith.edu>
2023-04-18 12:36:05 -0400

    slurm submit script

---
+++ submit_sigma.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=nonlinearsigma_test        # Job name
+#SBATCH --mail-type=END,FAIL                  # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=cberger@smith.edu         # Where to send mail	
+#SBATCH --partition=phyq                      # Which partition to use
+#SBATCH --ntasks=1                            # Run on a single CPU
+#SBATCH --mem=1gb                             # Job memory request
+#SBATCH --time=00:05:00                       # Time limit hrs:min:sec
+#SBATCH --output=nonlinearsigma_test_%j.log           # Standard output 
+#SBATCH --error=err_nonlinearsigma_test_%j.log   # Standard output and error log
+
+pwd; hostname; date
+
+echo "Running nonlinear sigma on single CPU core"
+
+nonlinearsigma inputs.txt
+
+date
