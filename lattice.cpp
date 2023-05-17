34e671f60aa6a5eb4d357fb438d76f24dcb93680
cberger <cberger@jupyterhub.smith.edu>
2023-05-17 12:22:15 -0400

    Fixed error in the cross product

--- lattice.cpp
+++ lattice.cpp
@@ -113,6 +113,6 @@ void cross_product(double vec1[3], double vec2[3],double (&cross_prod)[3]){
 #endif
     //calculates the cross product of two vectors
     cross_prod[0] = vec1[1]*vec2[2] - vec1[2]*vec2[1];
-    cross_prod[1] = vec1[0]*vec2[2] - vec1[2]*vec2[0];
+    cross_prod[1] = vec1[2]*vec2[0] - vec1[0]*vec2[2];
     cross_prod[2] = vec1[0]*vec2[1] - vec1[1]*vec2[0];
 }
