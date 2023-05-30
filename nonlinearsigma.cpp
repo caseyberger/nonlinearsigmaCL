50edfd9b7f6a6cebeedb7fbefb0c4f7595f4723e
cberger <cberger@jupyterhub.smith.edu>
2023-05-30 11:59:33 -0400

    typos

--- nonlinearsigma.cpp
+++ nonlinearsigma.cpp
@@ -213,7 +213,7 @@ void read_in_inputs(int argc, char *argv[],int &len, int &num, int &ntherm, int 
     cout << "Function: read_in_inputs" << endl;
 #endif
     string str, filename;
-    const int n_params = 4;
+    const int n_params = 5;
     string inputs [n_params] = {"L","beta", "itheta", "ntherm","nMC"};//read in keywords for parameters
     if (argc != 2){ //exits if input file is not given
         cerr << "Usage: ./nonlinearsigma input.txt"<< endl << "Exiting program" << endl;
