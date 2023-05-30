3588fe993d0ae9a869b0e93fbcdbdbc910ad0ed9
cberger <cberger@jupyterhub.smith.edu>
2023-05-30 11:24:55 -0400

    Added new header that contains special math operations dot and cross for three component vector

---
+++ mathlib.cpp
@@ -0,0 +1,19 @@
+//Casey Berger
+//Created: May 29, 2023
+//Edited: May 29, 2023
+//all useful math operations stored here
+
+#include "mathlib.h"
+
+double dot(double a[3], double b[3]){
+    double s = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+    return s;
+}
+
+double* cross(double a[3], double b[3]){
+    double c[3];
+    c[0] = a[1]*b[2] - a[2]*b[1];
+    c[1] = a[2]*b[0] - a[0]*b[2];
+    c[2] = a[0]*b[1] - a[1]*b[0];
+    return c;
+}
---
+++ mathlib.h
@@ -0,0 +1,10 @@
+//Casey Berger
+//Created: May 29, 2023
+//Edited: May 29, 2023
+//header for math library
+
+#ifndef MATH_H
+#define MATH_H
+double dot(double a[3], double b[3]);
+double* cross(double a[3], double b[3]);
+#endif
