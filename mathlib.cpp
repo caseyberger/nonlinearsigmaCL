077cf459838da1e1906137b45efc466454b3d1fc
cberger <cberger@jupyterhub.smith.edu>
2023-05-30 12:34:51 -0400

    debugging

--- mathlib.cpp
+++ mathlib.cpp
@@ -11,7 +11,7 @@ double dot(double a[3], double b[3]){
 }
 
 double* cross(double a[3], double b[3]){
-    double c[3];
+    static double c[3];
     c[0] = a[1]*b[2] - a[2]*b[1];
     c[1] = a[2]*b[0] - a[0]*b[2];
     c[2] = a[0]*b[1] - a[1]*b[0];
