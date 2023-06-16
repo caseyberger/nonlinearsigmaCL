bff5517ce7c9603740656ff05d747b002d41d3f6
cberger <cberger@jupyterhub.smith.edu>
2023-06-16 13:33:26 -0400

    Debugging custom type names

--- mathlib.h
+++ mathlib.h
@@ -1,10 +1,10 @@
 //Casey Berger
 //Created: May 29, 2023
-//Last edited: May 30, 2023
+//Last edited: June 16, 2023
 //header for math library
 
 #ifndef MATH_H
 #define MATH_H
-double dot(double a[3], double b[3]);
-double* cross(double a[3], double b[3]);
+double dot(std::array<double, 3> a, std::array<double, 3> b);
+std::array<double, 3> cross(std::array<double, 3> a, std::array<double, 3> b);
 #endif
